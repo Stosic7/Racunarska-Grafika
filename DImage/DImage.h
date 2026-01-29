a7e942e0a797c5e3864c1d7ce71a28a73ef3c6d3
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2026-01-29 07:18:05 +0100

    Refactor DImage.h for consistency and formatting

--- DImage/DImage.h
+++ DImage/DImage.h
@@ -1,6 +1,5 @@
 #pragma once
-#include <cstddef>
-
+ 
 #define DIB_HEADER_MARKER   ((WORD) ('M' << 8) | 'B')
 
 class DImage
@@ -13,20 +12,18 @@ public:
 	bool	Load(CString fileName); // Ucitava sliku iz datoteke cije se ime navodi
 	bool	Save(CString fileName); // Upisuje sliku u datoteku cije se ime navodi
 	void	Draw(CDC* pDC, CRect rcImg, CRect rcDC); // Iscrtava sliku u datom DC-ju
+	void	DrawTransparent(CDC* pDC, CRect rcImg, CRect rcDC, COLORREF color); // Iscrtava sliku u datom DC-ju sa zadatom bojom kao providnom
 
-	int		Width() { return m_nWidth; }	// Sirina u pikselima
-	int		Height() { return m_nHeight; } // Visina u pikselima
-	int		BPP() { return m_nBPP; }						// Broj bajtova po pikselu
-
-	bool	isValid() { return m_pBuf != NULL; }
-
+	int		Width(){return m_nWidth;}	// Sirina u pikselima
+	int		Height(){return m_nHeight;} // Visina u pikselima
+	int		BPP(){return m_nBPP;}						// Broj bajtova po pikselu
+	
+	bool	isValid(){return m_pBuf!=NULL;}
+	
 	// Direktne izmene
-	unsigned char* GetDIBBits() { return m_pBuf; } // Vraca pointer na prvi bajt sa pikselima
+	unsigned char* GetDIBBits(){return m_pBuf;} // Vraca pointer na prvi bajt sa pikselima
 	void	Update();		// Pozvati nakon direktne izmene bafera.
-
-	CBitmap* GetBitmap() { return m_pBmp; };
-	void DrawTransparent(CDC* pDC, CRect srcRc, CRect dstRc, COLORREF clrTransparent);
-
+	
 protected:
 	void Flip();
 	void Convert(int oldBPP, int newBPP);
@@ -34,8 +31,7 @@ protected:
 	int m_nWidth;
 	int m_nHeight;
 	int m_nBPP;
-	int GetScanlineWidth() { return ((m_nWidth * m_nBPP + 3) / 4) * 4; }
+	int GetScanlineWidth(){return ((m_nWidth*m_nBPP+3)/4)*4;}
 	unsigned char* m_pBuf;
 	CBitmap* m_pBmp;
-
 };
